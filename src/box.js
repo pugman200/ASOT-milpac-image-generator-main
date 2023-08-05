aaf8edbb2291f87659ba1be82201d459c14f24a0
Arnav Mishra <arnavdrsk7633@gmail.com>
2023-08-05 05:52:18 +0530

    box logic

--- src/box.js
+++ src/box.js
@@ -12,7 +12,7 @@ const main = async (data) => {
     const ctx = canvas.getContext("2d");
     ctx.drawImage(
       await loadImage(
-        __dirname + "/../medal-box-images/Blank_Medal_Case_Template.png"
+        __dirname + "/..\\medal-box-images\\Blank_Medal_Case_Template.png"
       ),
       0,
       0,
@@ -42,13 +42,31 @@ const main = async (data) => {
     //left  - 85,98
     //right - 795, 99
     //one medal is 70x153
-    const left = 85;
-    const right = 795;
-    const top = 98;
+    const left = 102;
+    const right = 794;
+    const top = 102;
+    const length = right - left;
+    const fixedCentre = length / 2;
+    const medalLength = (foundMedals.length * 74) / 3;
+    const centreDifference = length / 2 - medalLength / 2;
+    const start = right - centreDifference;
+    let counter = 0;
+    for (const medal of foundMedals) {
+      //draw the medals so that the centre of medalLength is the centre of the box
+      const medalImage = await loadImage(
+        __dirname + `\\..\\medal-box-images\\medals\\${medal}.png`
+      );
+      console.log(start - counter * (74 / 3));
+      //each medal overlaps the previous medal by 1/3 of its length
+      ctx.drawImage(medalImage, start - counter * (74 / 3), top, 74, 155);
+      counter++;
+    }
+    const buffer = canvas.toBuffer("image/png");
+    fs.writeFileSync(__dirname + "\\..\\medal-box-images\\box.png", buffer);
   } catch (err) {
     console.log(err);
   }
 };
 main({
-  medals: ["3 Year", "Founders", "Diplomat", "Public Relation", "1 Year"],
+  medals: ["3 Year", "Founders", "Diplomat", "Public Relations", "1 Year"],
 });
